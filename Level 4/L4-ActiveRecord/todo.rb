59e01882747ee12aa24f29464ecfa59bf123a448
Karthick-sketch <karthick246.kh@outlook.com>
2021-05-26 23:21:25 +0530

    add comments in todo.rb file

--- Level 4/L4-ActiveRecord/todo.rb
+++ Level 4/L4-ActiveRecord/todo.rb
@@ -3,27 +3,29 @@ require "active_record"
 # todo.rb
 class Todo < ActiveRecord::Base
   def overdue?
-    due_date < Date.today # check the to-do is overdue or not
+    due_date < Date.today
   end
 
   def due_today?
-    due_date == Date.today # check the to-do is today or not
+    due_date == Date.today
   end
 
   def due_later?
-    due_date > Date.today # check the to-do is later day or not
+    due_date > Date.today
   end
 
   def self.overdue
+    # Filter to-dos that are overdue
     all.filter { |todo| todo.overdue? }
-    # puts all.first.overdue?
   end
 
   def self.due_today
+    # Filter to-dos that are due today
     all.filter { |todo| todo.due_today? }
   end
 
   def self.due_later
+    # Filter to-dos that are due later
     all.filter { |todo| todo.due_later? }
   end
 
@@ -38,32 +40,22 @@ class Todo < ActiveRecord::Base
   end
 
   def self.show_list
-    # prints the list of todos as per the above format.
-    puts "My Todo-list\n\n"
-
-    puts "Overdue\n"
-    puts to_displayable_list(overdue)
-    puts "\n\n"
-
-    puts "Due Today\n"
-    puts to_displayable_list(due_today)
-    puts "\n\n"
-
-    puts "Due Later\n"
-    puts to_displayable_list(due_later)
-    puts "\n\n"
+    # print the list of todos
+    puts ["My Todo-list",
+          "Overdue\n" + to_displayable_list(overdue),
+          "Due Today\n" + to_displayable_list(due_today),
+          "Due Later\n" + to_displayable_list(due_later)].join("\n\n\n")
   end
 
   def self.add_task(hash)
-    # it will take a Ruby hash as parameter, containing todo_text and due_in_days.
+    # take a Ruby hash as parameter, containing todo_text and due_in_days.
     Todo.create!(todo_text: hash[:todo_text], due_date: Date.today + hash[:due_in_days], completed: false)
   end
 
   def self.mark_as_complete(id)
-    # set its completed to true
     todo = Todo.find(id)
-    todo.completed = true
-    todo.save
+    # set its completed to true
+    todo.completed = true; todo.save
     todo
   end
 end
