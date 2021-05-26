20dbe2f23e5a6aea57ad5f94c5a29d48b0092b9c
Karthick-sketch <karthick246.kh@outlook.com>
2021-05-26 12:19:03 +0530

    Replace encrypted file when decrypt

--- Level 4/decrypt.rb
+++ Level 4/decrypt.rb
@@ -4,6 +4,7 @@ source_file = ARGV[0]
 password = ARGV[1]
 decrypted = AES.decrypt(File.read(source_file), password)
 
-target_file = "#{source_file.slice(0, source_file.size - 4)}.dec"
+target_file = source_file.slice(0, source_file.size - 4)
 File.open(target_file, "wb") { |f| f.write(decrypted) }
+system "rm " + source_file
 puts "File decrypted to #{target_file} with password #{password}"
--- Level 4/encrypt.rb
+++ Level 4/encrypt.rb
@@ -6,4 +6,5 @@ encrypted = AES.encrypt(File.read(source_file), password)
 
 target_file = "#{source_file}.enc"
 File.open(target_file, "wb") { |f| f.write(encrypted) }
+system "rm " + source_file
 puts "File encrypted to #{target_file} with password #{password}"
